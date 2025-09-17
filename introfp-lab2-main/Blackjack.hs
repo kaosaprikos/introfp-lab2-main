1b387d71941d7597a01f9e3804a39c99575ae52d
AlbaMoriWallin <93259474+AlbaMoriWallin@users.noreply.github.com>
2025-09-17 12:01:39 +0200

    Update Blackjack.hs
    De vi gjort än så länge

--- introfp-lab2-main/Blackjack.hs
+++ introfp-lab2-main/Blackjack.hs
@@ -19,37 +19,95 @@ import Test.QuickCheck hiding (shuffle)
 
 -- Task A1 --
 
+-- Card examples
+aCard1 :: Card
+aCard1 = Card (Numeric 7) Clubs
+
+aCard2 :: Card
+aCard2 = Card Ace Diamonds
+
+-- Hand examples, takes cards from our Card examples. 
+aHand :: Hand
+aHand = (aCard1 : (aCard2 : []))
+
+-- Task A1 --
+
 hand2 :: Hand
-hand2 = [Card (Numeric 2) Hearts, Card Jack Spades]
+hand2 = [Card (Numeric 5) Hearts, Card Jack Spades, Card King Hearts]
 
+hand3 :: Hand
+hand3 = [Card King Hearts, Card (Numeric 3) Clubs, Card (Numeric 7) Diamonds]
+
+-- size hand2
+-- = size (Card (Numeric 2) Hearts : (Card Jack Spades : []))
+-- = size [Card (Numeric 2) Hearts, Card Jack Spades]
+-- = 1 + size [Card Jack of Spades]
+-- = 1 + (1 + size [])
+-- = 1 + (1 + 0)
+-- = 2
+
+-- Returns list where every element is a different step of the function size using aHand
 sizeSteps :: [Int]
-sizeSteps = undefined
-
+sizeSteps = 
+            [ size aHand
+            , size (Card (Numeric 2) Hearts : (Card Jack Spades : []))
+            , 1 + size (Card Jack Spades : [])
+            , 1 + 1 + (size [])
+            , 1 + 1 + 0
+            , 1 + 1
+            , 2
+            ]
+            
 -- Task A2 --
 
+-- displayCard :: Card -> String
+-- displayCard (Card x Diamonds) = show x ++ "diamonds"
+
+
+-- displays cards rank and suit as strings
 display :: Hand -> String
-display = undefined
+display [] = ""
+display ((Card (Numeric r) s):xs) = show r ++ " of " ++ show s ++ ", " ++ display xs
+display ((Card r s):xs)  = show r ++ " of " ++ show s ++ ", " ++ display xs
+
+
+
 
 -- Task A3 --
 
+-- Adds the value of each card in a hand and sums them up
+valueHand :: Hand -> Int 
+valueHand [] = 0
+valueHand ((Card (Numeric x) _):xs) = x + valueHand xs
+valueHand ((Card Ace _):xs) = 11 + valueHand xs
+valueHand ((Card x _):xs) = 10 + valueHand xs
+
+--Checks if hand value is bigger than 21 and then subtracts 10 points for each ace (changes ace value from 11 to 1)
 value :: Hand -> Int
-value = undefined
+value xs = if valueHand xs > 21 then valueHand xs - sum[10 | Card Ace x <- xs] else valueHand xs
+ 
 
 -- Task A4 --
---
-gameOver :: Hand -> Bool
-gameOver = undefined
 
+--checks if the player is bust, has more than 21 points
+gameOver :: Hand -> Bool
+gameOver xs = value xs > 21
+
+--decides if Guest or Bank wins depending om their final value and who has gone bust
 winner :: Hand -> Hand -> Player
-winner = undefined
+winner g b 
+ | value b > 21 && value g < 21 = Guest 
+ | value g > value b && value g <= 21 = Guest
+ | otherwise = Bank
 
 --------------------------------------------------------------------------------
 -- Part B
 ---------------------------------------------------------------------------------
 
 -- Task B1 --
+
 fullDeck :: Deck
-fullDeck = undefined
+fullDeck = [(Card r s) | r <- [Numeric x | x <- [2..10]] ++ [Jack, Queen, King, Ace], s <- [Hearts, Spades, Diamonds, Clubs]]
 
 prop_size_fullDeck :: Bool
 prop_size_fullDeck = size fullDeck == 52
